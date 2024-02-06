cbe369154e286ddd1bb241541183419cd880134b
Marit <93144973+tiram-isu@users.noreply.github.com>
2024-02-06 19:23:56 +0100

    Update PoseDataUnitySocket.cs

--- Assets/Scripts/PoseDataUnitySocket.cs
+++ Assets/Scripts/PoseDataUnitySocket.cs
@@ -14,14 +14,14 @@ public class PoseDataUnitySocket
     int connectionPort = 13000;
     string ip = "127.0.0.1";
     public TcpClient client;
-    int bufferSize = 4096;  // 32,768 bytes - max file people will be detected, equals ~ 15kByte
+    int bufferSize = 4096;  // 32,768 bytes - max file size for two detected people
 
     public void StartServer()
     {
         server = new TcpListener(IPAddress.Parse(ip), connectionPort);
         server.Start();
         Debug.Log("Server started");
-        client = server.AcceptTcpClient(); // makes unity crash sometimes
+        client = server.AcceptTcpClient();
         Debug.Log("Client connected");
         Thread thread = new Thread(() => StartClientConnection());
         thread.Start();
@@ -40,28 +40,16 @@ public class PoseDataUnitySocket
 
         while ((byteCount = stream.Read(buffer, 0, buffer.Length)) > 0)
         {
-            //stream.Read(buffer, 0, buffer.Length);
-            // float[] floatArray = new float[byteCount / 4];
-            int maxKeyPoints = 150; // ensures, that only keypoints for two people are being used (Body 25)
-            float[] floatArray = new float[maxKeyPoints];
-            int maxByteCount = 4 * maxKeyPoints; // one float = 4 byte, we need 150 float keypoints from the buffer filled with floats as raw bytes
+            int maxKeyPoints = 150; // ensures that only keypoints for two people are being used (Body 25 -> 75 values per person)
+            float[] floatArray = new float[maxKeyPoints]; // create empty array with 150 values
+            int maxByteCount = 4 * maxKeyPoints; // one float = 4 byte
+            
             for (int i = 0; i < maxByteCount; i += 4)
             {
-                floatArray[i / 4] = BitConverter.ToSingle(buffer, i);
+                floatArray[i / 4] = BitConverter.ToSingle(buffer, i); // convert bytes to float and write in array
             }
-            PoseDataAccess.SetPoseData(floatArray);
-
-            /*String output = "";
-            foreach (var item in floatArray)
-            {
-                output += item.ToString() + "; ";
-                //Debug.Log(item.ToString());
-            }
-            Debug.Log("output: " + output);*/
-
-            //Debug.Log("[{0}]", string.Join(", ", PoseDataAccess.GetPoseData()));
-            //Debug.Log(PoseDataAccess.GetPoseData() );
-
+            
+            PoseDataAccess.SetPoseData(floatArray); // set array
         }
         Console.WriteLine("Client disconnected");
     }
