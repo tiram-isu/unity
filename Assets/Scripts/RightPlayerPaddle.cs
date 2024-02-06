3cf212aab6ce3681a841543200e76a954efeb2ce
Marit <93144973+tiram-isu@users.noreply.github.com>
2024-02-06 19:26:06 +0100

    Update RightPlayerPaddle.cs

--- Assets/Scripts/RightPlayerPaddle.cs
+++ Assets/Scripts/RightPlayerPaddle.cs
@@ -13,29 +13,26 @@ public class RightPlayerPaddle : Paddle
     private Vector2 direction;
     private float normalizedRelativePosY;
 
-    private void Start()
-    {
-        /*UnityServer server = new UnityServer();
-        Debug.Log("Starting server...");
-        server.StartServer();*/
-    }
-
     private void Update()
     {
-        float[] rightPerson = PoseDataAccess.GetRightPersonPoseData();
-        float rightHand = rightPerson[13];
-        float rightShoulder = rightPerson[7];
-        float hip = rightPerson[25];
-        float armLength = Math.Abs(hip - rightShoulder) / 2;
+        float[] rightPerson = PoseDataAccess.GetRightPersonPoseData(); // get all keypoints of the rightmost person 
+        
+        float rightHand = rightPerson[13]; // get coordinates of right hand
+        float rightShoulder = rightPerson[7]; // get coordinates of right shoulder
+        float hip = rightPerson[25]; // get coordinates of hip
+        
+        float armLength = Math.Abs(hip - rightShoulder) / 2; // calculate approximate arm length as distance from hip to shoulder
+        
         if (armLength == 0)
-            return;
-        normalizedRelativePosY = (rightShoulder - rightHand) / armLength;
-        direction = new Vector2(0, adjustedVelocity(normalizedRelativePosY));
+            return; // return if there is no data yet
+            
+        normalizedRelativePosY = (rightShoulder - rightHand) / armLength; // normalize the distance between shoulder and hand with the arm length
+        direction = new Vector2(0, adjustedVelocity(normalizedRelativePosY)); // set paddle direction
     }
 
     private float adjustedVelocity(float val)
     {
-        return (float)(Math.Pow(val, 3) * paddleSpeed);
+        return (float)(Math.Pow(val, 3) * paddleSpeed); // adjust the velocity by using a cubic function
     }
 
     private void FixedUpdate()
