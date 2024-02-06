b5a20bf85ba39175af9ba647409c35b4429c3cb9
Marit <93144973+tiram-isu@users.noreply.github.com>
2024-02-06 19:24:41 +0100

    Update LeftPlayerPaddle.cs

--- Assets/Scripts/LeftPlayerPaddle.cs
+++ Assets/Scripts/LeftPlayerPaddle.cs
@@ -13,29 +13,26 @@ public class LeftPlayerPaddle : Paddle
     private Vector2 direction;
     private float normalizedRelativePosY;
 
-    private void Start()
-    {
-        /*UnityServer server = new UnityServer();
-        Debug.Log("Starting server...");
-        server.StartServer();*/
-    }
-
     private void Update()
     {
-        float[] leftPerson = PoseDataAccess.GetLeftPersonPoseData();
-        float leftHand = leftPerson[22];
-        float leftShoulder = leftPerson[16];
-        float hip = leftPerson[25];
-        float armLength = Math.Abs(hip - leftShoulder) / 2;
+        float[] leftPerson = PoseDataAccess.GetLeftPersonPoseData(); // get all keypoints of the leftmost person 
+        
+        float leftHand = leftPerson[22]; // get coordinates of left hand
+        float leftShoulder = leftPerson[16]; // get coordinates of left shoulder
+        float hip = leftPerson[25]; // get coordinates of hip
+        
+        float armLength = Math.Abs(hip - leftShoulder) / 2; // calculate approximate arm length as distance from hip to shoulder
+        
         if (armLength == 0)
-            return;
-        normalizedRelativePosY = (leftShoulder - leftHand) / armLength;
-        direction = new Vector2(0, adjustedVelocity(normalizedRelativePosY));
+            return; // return if there is no data yet
+            
+        normalizedRelativePosY = (leftShoulder - leftHand) / armLength; // normalize the distance between shoulder and hand with the arm length
+        direction = new Vector2(0, adjustedVelocity(normalizedRelativePosY)); // set paddle direction
     }
 
     private float adjustedVelocity(float val)
     {
-        return (float)(Math.Pow(val, 3) * paddleSpeed);
+        return (float)(Math.Pow(val, 3) * paddleSpeed); // adjust the velocity by using a cubic function
     }
 
     private void FixedUpdate()
